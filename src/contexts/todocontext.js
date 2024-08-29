9ec28f3c3d8ad75994008ee7ad862c3505e45de9
shzad78 <shzad78@gmail.com>
2024-08-29 12:09:12 +0200

    new changes

--- src/contexts/todocontext.js
+++ src/contexts/todocontext.js
@@ -1,1 +1,8 @@
 import { useContext, createContext } from "react";
+
+export const TodoContext = createContext({});
+
+export const Todoprovider = TodoContext.Provider;
+export const useTodo = () => {
+  return useContext(TodoContext);
+};
