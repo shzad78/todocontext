d83eba0398eed357d32ceae24221ef327857ec3f
shzad78 <shzad78@gmail.com>
2024-08-29 13:09:47 +0200

    add delete update todo

--- src/contexts/todocontext.js
+++ src/contexts/todocontext.js
@@ -1,8 +1,20 @@
 import { useContext, createContext } from "react";
 
-export const TodoContext = createContext({});
+export const TodoContext = createContext({
+  todo: [
+    {
+      id: 1,
+      todo: "1st message",
+      completed: false,
+    },
+  ],
+  addTodo: (todo) => {},
+  updateTodo: (id, todo) => {},
+  deleteTodo: (id) => {},
+  toggleComplete: (id) => {},
+});
 
-export const Todoprovider = TodoContext.Provider;
+export const TodoProvider = TodoContext.Provider;
 export const useTodo = () => {
   return useContext(TodoContext);
 };
